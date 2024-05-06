4e757804f7eca5e48922ceb5478e493fafb9644c
YtongXie <44012746+YtongXie@users.noreply.github.com>
2024-05-06 19:42:22 +0930

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from distutils.core import setup
+
+
+setup(
+    name="medim",
+    description="Masked medical Image Modelling (MedIM).",
+    version="0.1",
+    packages=setuptools.find_packages(),
+    author="Yutong Xie",
+    author_email="yutong.xie678@gmail.com"
+)
